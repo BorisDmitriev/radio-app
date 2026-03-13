3378511465b888fed6f899cc3072082392d88362
Lukas Börger <lukas0606@outlook.de>
2026-03-13 17:44:45 +0100

    now playing

--- src/app/features/now-playing/now-playing.component.ts
+++ src/app/features/now-playing/now-playing.component.ts
@@ -1,11 +1,22 @@
+import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { NowPlayingService } from '../../core/services/now-playing.service';
+import {NowPlaying, NowPlayingView} from '../../shared/models/now-playing.model';
 
 @Component({
   selector: 'app-now-playing',
-  imports: [],
+  standalone: true,
+  imports: [NgIf, AsyncPipe, DatePipe],
   templateUrl: './now-playing.component.html',
   styleUrl: './now-playing.component.scss'
 })
 export class NowPlayingComponent {
+  readonly nowPlaying$;
 
+  constructor(private readonly nowPlayingService: NowPlayingService) {
+    this.nowPlaying$ = this.nowPlayingService.nowPlaying$.pipe(
+      map((): NowPlayingView | null => this.nowPlayingService.getNowPlayingView())
+    );
+  }
 }
