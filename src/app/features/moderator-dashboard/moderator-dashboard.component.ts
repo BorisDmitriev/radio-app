b3bbb4b6d0dcf28493a6da9b1645e76f00ec25e0
BorisDmitriev <bdmitriev83@gmail.com>
2026-03-15 14:46:53 +0100

    implementiert Moderator-Bewertung und Playlist-Bewertung im Dashboard sowie Unit-Tests

--- src/app/features/moderator-dashboard/moderator-dashboard.component.ts
+++ src/app/features/moderator-dashboard/moderator-dashboard.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+import {
+  ModeratorDashboardService,
+  DashboardStats,
+  Moderator,
+  Playlist
+} from '../../core/services/moderator-dashboard.service';
+
+import { ModeratorRating } from '../../shared/models/moderatorRating.model';
+import { PlaylistRating } from '../../shared/models/playlistRating.model';
 
 @Component({
   selector: 'app-moderator-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './moderator-dashboard.component.html',
   styleUrl: './moderator-dashboard.component.scss'
 })
+
 export class ModeratorDashboardComponent {
 
+  stats$: Observable<DashboardStats<ModeratorRating>>;
+  moderators$: Observable<Moderator[]>;
+
+  playlistStats$: Observable<DashboardStats<PlaylistRating>>;
+  playlists$: Observable<Playlist[]>;
+
+  constructor(private dashboardService: ModeratorDashboardService) {
+    this.stats$ = this.dashboardService.stats$;
+    this.moderators$ = this.dashboardService.moderators$;
+
+    this.playlistStats$ = this.dashboardService.playlistStats$;
+    this.playlists$ = this.dashboardService.playlists$;
+  }
+
+  onModeratorChange(event: Event) {
+    const moderatorId = (event.target as HTMLSelectElement).value;
+    this.dashboardService.setModerator(moderatorId);
+  }
+
+  onPlaylistChange(event: Event) {
+    const playlistId = (event.target as HTMLSelectElement).value;
+    this.dashboardService.setPlaylist(playlistId);
+  }
+
+  // Bewertung runden
+  getRoundedRating(value: number): number {
+    return Math.round(value);
+  }
 }
